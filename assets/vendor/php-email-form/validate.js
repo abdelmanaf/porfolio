5471841761a13234d12396f123c44c2528d65913
abdelmanaf <ourotoumanaf6@gmail.com>
2021-12-15 15:14:10 -0500

    complete the contact me form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -60,16 +60,16 @@
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      if (data.trim() != 'OK') {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
     })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
+    // .catch((error) => {
+    //   displayError(thisForm, error);
+    // });
   }
 
   function displayError(thisForm, error) {
